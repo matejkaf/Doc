cd06b1a975b87b14a4ae17986d2602a7b9effd53
Franz Matejka <matejkaf@gmail.com>
2021-07-14 15:14:38 +0200

    upload 14.07.21 15:14

--- present.js
+++ present.js
@@ -1,28 +1,47 @@
 console.log("test")
-let position = 0;
+let position = 0
+let presentation_mode = false
+let hot_key = 'p'
+let hot_key_pressed = false
+
 // process keys
 document.addEventListener('keydown', keydownHandler);
 
 function keydownHandler(e) {
-  if(e.key==' ') {
-    console.log("Space")
-    let main_element = document.getElementsByTagName('main')[0];
-    main_element.children[position].style.visibility = "visible"
-    position++
+  if (!presentation_mode) {
+    if (e.key == hot_key) {
+      console.log(e.key)
+      if (hot_key_pressed) {
+        hot_key_pressed = false
+        presentation_mode = true
+        hide_all_first_level_elements()
+        position = 0
+      } else {
+        hot_key_pressed = true
+      }
+    }
+  } else {
+    // in presentation mode
+    if (e.key == ' ') {
+      console.log("Space")
+      let main_element = document.getElementsByTagName('main')[0];
+      let element_to_show = main_element.children[position]
+      position++
+      element_to_show.style.visibility = "visible"
+      element_to_show.scrollIntoView(false) // alignToTop=false
+      e.preventDefault() // no scrolling by space
+    }
+
   }
 }
 
-// hide all
+function hide_all_first_level_elements() {
+  let main_element = document.getElementsByTagName('main')[0];
+  main_element.children[0].style.visibility = "hidden"
 
-//document.body.children[0].style.visibility = "hidden"
-
-let main_element = document.getElementsByTagName('main')[0];
-main_element.children[0].style.visibility = "hidden"
-
-for(let i=0; i<main_element.children.length; i++) {
-  let el = main_element.children[i]
-  console.log(el)
-  console.log(el.nodeName)
-  console.log(el.nodeType)
-  el.style.visibility = "hidden"
+  for (let i = 0; i < main_element.children.length; i++) {
+    let el = main_element.children[i]
+    //console.log(el.nodeName) // P, UL, SCRIPT
+    el.style.visibility = "hidden"
+  }
 }
