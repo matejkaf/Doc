9101ee7893e79fb188f9c8bbef03ad007e8bdb0a
Franz Matejka <matejkaf@gmail.com>
2021-07-14 15:38:10 +0200

    upload 14.07.21 15:38

--- present.js
+++ present.js
@@ -24,18 +24,45 @@ function keydownHandler(e) {
     // in presentation mode
     if (e.key == ' ') {
       console.log("Space")
-      let main_element = document.getElementsByTagName('main')[0];
-      let element_to_show = main_element.children[position]
       position++
-      element_to_show.style.visibility = "visible"
-      element_to_show.scrollIntoView(false) // alignToTop=false
+      let element_to_show = setVisibilityOnElementNr(position,"visible")
+      scrollIfNecessary(element_to_show)
       e.preventDefault() // no scrolling by space
+    } else if (event.keyCode==8) {
+      console.log("backspace")
+      if(position>0) {
+        setVisibilityOnElementNr(position,"hidden")
+        position--
+        scrollIfNecessary(getElementNr(position))
+      }
+      e.preventDefault()
     }
 
   }
 }
 
-function hide_all_first_level_elements() {
+function scrollIfNecessary(el) 
+{
+  if (!isElementInViewport(el)) {
+    el.scrollIntoView(false) // alignToTop=false
+  }
+}
+
+function getElementNr(nr) 
+{
+  let main_element = document.getElementsByTagName('main')[0];
+  return main_element.children[nr]
+}
+
+function setVisibilityOnElementNr(nr,visibility) 
+{
+  let element_to_set = getElementNr(nr)
+  element_to_set.style.visibility = visibility
+  return element_to_set
+}
+
+function hide_all_first_level_elements() 
+{
   let main_element = document.getElementsByTagName('main')[0];
   main_element.children[0].style.visibility = "hidden"
 
@@ -44,4 +71,16 @@ function hide_all_first_level_elements() {
     //console.log(el.nodeName) // P, UL, SCRIPT
     el.style.visibility = "hidden"
   }
+}
+
+function isElementInViewport(el) 
+{
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /* or $(window).height() */
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth) /* or $(window).width() */
+  );
 }
