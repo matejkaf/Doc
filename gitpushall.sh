79f7909a69e87051877bb0b95effd6f6443d49b7
Franz Matejka <matejkaf@gmail.com>
2021-09-26 16:25:31 +0200

    upload 26.09.21 16:25

--- gitpushall.sh
+++ gitpushall.sh
@@ -5,7 +5,7 @@ echo "= Create HTML Index of md Files ="
 echo "================================="
 
 #./Python/Scripts/19_md_manager/htmltags.py "" "C_1" "C_2" "SYTG_2_Linux" "Cybersecurity_2" "Database" "challenge" 
-./Python/Scripts/19_md_manager/htmltags.py "" "4BHELS" "3AHITS" "2AHITS" "1AHITS" "2AHELS" "1CHELS"
+./Python/Scripts/19_md_manager/htmltags.py "" "4BHELS" "3AHITS" "2AHITS" "1AHITS" "1CHELS"
 
 echo "Publish to gitpages"
 git add --all
